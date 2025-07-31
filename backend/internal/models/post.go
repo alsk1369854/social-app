7318df83f587c7b6da82088f28edfa84b79e707b
alsk1369854 <alsk1369854@gmail.com>
2025-07-31 21:41:11 +0800

    chore: update docker-compose.yml

--- backend/internal/models/post.go
+++ backend/internal/models/post.go
@@ -1,9 +1,17 @@
 package models
 
+import "github.com/google/uuid"
+
 type Post struct {
 	TableModel
 	PostBase
 }
 
 type PostBase struct {
+	AuthorID  uuid.UUID `gorm:"not null"`
+	Author    *User     `gorm:"foreignKey:AuthorID"`
+	ImageURL  *string   `gorm:"default:null"`
+	Content   string    `gorm:"not null"`
+	CreatedAt int64     `gorm:"autoCreateTime"`
+	UpdatedAt int64     `gorm:"autoUpdateTime"`
 }
